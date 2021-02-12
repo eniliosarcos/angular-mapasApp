6c9fbef5b156ffc058ea9444561e4fd7a122c6db
eniliosarcos <eniliosarcos1993@gmail.com>
2021-02-11 22:42:51 -0400

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiZW5pbGlvc2FyY29zIiwiYSI6ImNrbDFoaWpoODB4Z2UycHVpaHUxZ3Z2dnoifQ._A344xKmIPa5Y00W73kgWA'
 };
